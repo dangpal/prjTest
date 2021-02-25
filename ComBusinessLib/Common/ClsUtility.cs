9517f3885a18d2628c1dbc2b5a7b44ea7044f1ba
help <help@onehp.co.kr>
2021-02-25 13:59:12 +0900

    젠킨스테스트

--- ComBusinessLib/Common/ClsUtility.cs
+++ ComBusinessLib/Common/ClsUtility.cs
@@ -8,6 +8,6 @@ namespace BlazorWebApi.Common
 {
     public class ClsUtility
     {
-        public  static string strTest = "테스트 입니다.";
+        public  static string strTest = "테스트 입니다.안녕하세요반갑습니다.";
     }
 }
